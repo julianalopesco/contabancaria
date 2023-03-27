6ee9c94222a5f4b9c3786889650d715202c77098
Julianalopesco <julianalopesmf@gmail.com>
2023-03-27 08:35:01 -0300

    criacao class cores e conta bancaria

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,21 +1,30 @@
 package conta;
 
 import java.util.Scanner;
+import conta.model.Conta;
+import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
 
+		// Teste da Classe Conta
+				Conta c1 = new Conta(1, 123, 1, "Juliana Lopes", 10000.0f); //c1 = objeto 1 da classe conta
+				c1.visualizar();
+				c1.sacar(12000.0f);
+				c1.visualizar();
+				c1.depositar(5000.0f);
+				c1.visualizar();
 
 		Scanner leia = new Scanner(System.in);
 
 		int opcao;
 
 		while (true) {
-
+			System.out.println(Cores.TEXT_BLUE_BOLD + Cores.ANSI_YELLOW_BACKGROUND_BRIGHT);
 			System.out.println("*****************************************************");
 			System.out.println("                                                     ");
-			System.out.println("                BANCO DO BRAZIL COM Z                ");
+			System.out.println("                BANCO PAULISTANO                     ");
 			System.out.println("                                                     ");
 			System.out.println("*****************************************************");
 			System.out.println("                                                     ");
@@ -31,51 +40,51 @@ public class Menu {
 			System.out.println("                                                     ");
 			System.out.println("*****************************************************");
 			System.out.println("Entre com a opção desejada:                          ");
-			System.out.println("                                                     ");
+			System.out.println("                                                     "+ Cores.TEXT_RESET);
 
 			opcao = leia.nextInt();
 
 			if (opcao == 9) {
-				System.out.println("\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+				System.out.println(Cores.TEXT_BLUE_BOLD + "\nBanco Paulistano - O combustível da locomotiva do país");
 				leia.close();
 				System.exit(0);
 			}
 
 			switch (opcao) {
 				case 1:
-					System.out.println("Criar Conta\n\n");
+					System.out.println(Cores.TEXT_BLUE_BOLD + "Criar Conta\n\n");
 
 					break;
 				case 2:
-					System.out.println("Listar todas as Contas\n\n");
+					System.out.println(Cores.TEXT_BLUE_BOLD + "Listar todas as Contas\n\n");
 
 					break;
 				case 3:
-					System.out.println("Consultar dados da Conta - por número\n\n");
+					System.out.println(Cores.TEXT_BLUE_BOLD + "Consultar dados da Conta - por número\n\n");
 
 					break;
 				case 4:
-					System.out.println("Atualizar dados da Conta\n\n");
+					System.out.println(Cores.TEXT_BLUE_BOLD + "Atualizar dados da Conta\n\n");
 
 					break;
 				case 5:
-					System.out.println("Apagar a Conta\n\n");
+					System.out.println(Cores.TEXT_BLUE_BOLD + "Apagar a Conta\n\n");
 
 					break;
 				case 6:
-					System.out.println("Saque\n\n");
+					System.out.println(Cores.TEXT_BLUE_BOLD + "Saque\n\n");
 
 					break;
 				case 7:
-					System.out.println("Depósito\n\n");
+					System.out.println(Cores.TEXT_BLUE_BOLD + "Depósito\n\n");
 
 					break;
 				case 8:
-					System.out.println("Transferência entre Contas\n\n");
+					System.out.println(Cores.TEXT_BLUE_BOLD + "Transferência entre Contas\n\n");
 
 					break;
 				default:
-					System.out.println("\nOpção Inválida!\n");
+					System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n");
 					break;
 			}
 		}
