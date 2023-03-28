2d0a52ba67397c9c28431b71c41df35649aab3df
Julianalopesco <julianalopesmf@gmail.com>
2023-03-28 12:26:56 -0300

    acresc herancas, interfaces, inicio crud

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -3,18 +3,30 @@ package conta;
 import java.util.Scanner;
 import conta.model.Conta;
 import conta.util.Cores;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 
 public class Menu {
 
 	public static void main(String[] args) {
 
-		// Teste da Classe Conta
-				Conta c1 = new Conta(1, 123, 1, "Juliana Lopes", 10000.0f); //c1 = objeto 1 da classe conta
-				c1.visualizar();
-				c1.sacar(12000.0f);
-				c1.visualizar();
-				c1.depositar(5000.0f);
-				c1.visualizar();
+				
+		//teste da classe Conta Corrente
+			ContaCorrente cc1 = new ContaCorrente(2,123,1,"Mariana",1500.0f,1000.0f);
+			cc1.visualizar();
+			cc1.sacar(12000.0f);
+			cc1.visualizar();
+			cc1.depositar(5000.0f);
+			cc1.visualizar();
+			
+		// Teste da Classe Conta Poupança 
+			ContaPoupanca cp1 =  new ContaPoupanca(3,123,2,"Victor",100000.0f,15);
+			cp1.visualizar();
+			cp1.sacar(12000.0f);
+			cp1.visualizar();
+			cp1.depositar(5000.0f);
+			cp1.visualizar();
+
 
 		Scanner leia = new Scanner(System.in);
 
